9ad01c65e2696177a708f2bff16d2d614cec195a
mendalej <107884708+mendalej@users.noreply.github.com>
2023-11-27 16:16:50 -0600

    Add files via upload

---
+++ unit.py
@@ -0,0 +1,20 @@
+import csv
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+history = []
+
+def recs_history():
+    with open('book_history.csv', 'r') as file:
+        readers = csv.reader(file)
+        return [item for item in readers]
+    
+@app.route("/get_history", methods=["GET"])
+def get_history():
+    data = recs_history()
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
